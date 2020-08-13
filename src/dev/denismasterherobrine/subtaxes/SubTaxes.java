0f11699ab236f0f9f703efa6ac0f386f12fb33c3
DenisMasterHerobrine <27882185+denismasterherobrine@users.noreply.github.com>
2020-08-14 02:18:08 +0500

    Refactored database config a bit
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/dev/denismasterherobrine/subtaxes/SubTaxes.java
+++ src/dev/denismasterherobrine/subtaxes/SubTaxes.java
@@ -9,9 +9,7 @@ public class SubTaxes extends JavaPlugin {
 
     FileConfiguration config = getConfig();
     public String host_server, database_server, username_server, password_server;
-    public String host_weekly, database_weekly, username_weekly, password_weekly;
-    public String host_personal, database_personal, username_personal, password_personal;
-    public int port_server, port_weekly, port_personal;
+    public int port_server;
     public int PlayerHPInt, PlayerHPAdd, PlayerHPRemove;
 
     Date StartDate = new Date();
@@ -19,23 +17,11 @@ public class SubTaxes extends JavaPlugin {
     @Override
     public void onEnable(){
         // Fired when the server enables the plugin
-        config.addDefault("SQLUsernameServerTax"," ");
-        config.addDefault("SQLPasswordServerTax"," ");
-        config.addDefault("SQLDatabaseNameServerTax"," ");
-        config.addDefault("SQLHostServerTax"," ");
-        config.addDefault("SQLPortServerTax", 3306);
-
-        config.addDefault("SQLUsernameWeeklyTax"," ");
-        config.addDefault("SQLPasswordWeeklyTax"," ");
-        config.addDefault("SQLDatabaseNameWeeklyTax"," ");
-        config.addDefault("SQLHostWeeklyTax"," ");
-        config.addDefault("SQLPortWeeklyTax", 3306);
-
-        config.addDefault("SQLUsernamePersonalTax"," ");
-        config.addDefault("SQLPasswordPersonalTax"," ");
-        config.addDefault("SQLDatabaseNamePersonalTax"," ");
-        config.addDefault("SQLHostPersonalTax"," ");
-        config.addDefault("SQLPortPersonalTax", 3306);
+        config.addDefault("SQLUsername"," ");
+        config.addDefault("SQLPassword"," ");
+        config.addDefault("SQLDatabaseName"," ");
+        config.addDefault("SQLHost"," ");
+        config.addDefault("SQLPort", 3306);
 
         //Start of the 1st cycle
         config.addDefault("StartTime", StartDate);
@@ -50,23 +36,11 @@ public class SubTaxes extends JavaPlugin {
         config.options().copyDefaults(true);
         saveConfig();
 
-        host_server = config.getString("SQLHostServerTax");
-        port_server = config.getInt("SQLPortServerTax");
-        database_server = config.getString("SQLDatabaseNameServerTax");
-        username_server = config.getString("SQLUsernameServerTax");
-        password_server = config.getString("SQLPasswordServerTax");
-
-        host_weekly = config.getString("SQLHostWeeklyTax");
-        port_weekly = config.getInt("SQLPortWeeklyTax");
-        database_weekly = config.getString("SQLDatabaseNameWeeklyTax");
-        username_weekly = config.getString("SQLUsernameWeeklyTax");
-        password_weekly = config.getString("SQLPasswordWeeklyTax");
-
-        host_personal = config.getString("SQLHostPersonalTax");
-        port_personal = config.getInt("SQLPortPersonalTax");
-        database_personal = config.getString("SQLDatabaseNamePersonalTax");
-        username_personal = config.getString("SQLUsernamePersonalTax");
-        password_personal = config.getString("SQLPasswordPersonalTax");
+        host_server = config.getString("SQLHost");
+        port_server = config.getInt("SQLPort");
+        database_server = config.getString("SQLDatabaseName");
+        username_server = config.getString("SQLUsername");
+        password_server = config.getString("SQLPassword");
     }
 
     @Override
